d8bc41b785ab7008ecca85b04aaafc14766618e2
Mateusz Damian Chmurzyski <m.chmurzynski@stud.elka.pw.edu.pl>
2025-04-29 22:08:30 +0200

    P4 | poly kernel

--- SVM.py
+++ SVM.py
@@ -2,32 +2,66 @@ import numpy as np
 from cvxopt import matrix, solvers
 
 class SVM:
-    def __init__(self, kernel, gamma, C):
+    def __init__(self, kernel, gamma, C, coeff0=None, deg=None):
         self.alphas = None
         self.b = None
         self.K = None
+        self.w = None
+        self.b_alf = None
+        self.X = None
+        self.Y = None
         self.kernel = kernel
         self.gamma = gamma
         self.C = C
+        self.coeff0 = coeff0
+        self.deg = deg
 
     def fit(self, X, y):
+        X = np.array(X)
+        y = np.array(y)
+        self.X = X
+        self.Y = y
         if self.kernel == 'rbf':
-            X = np.array(X)
-            y = np.array(y)
             K = np.array([[np.exp(-self.gamma*np.sum((X[i] - X[j])**2))
                        for j in range(X.shape[0])]
                           for i in range(X.shape[0])])
-            self.K = K
-            Y = np.outer(y,y)
-            P = Y*K
-            q = -np.ones(X.shape[0])
-            A = y.reshape(1,-1)
-            G = np.vstack((-np.eye(X.shape[0]),np.eye(X.shape[0])))
-            h = np.hstack((np.zeros(X.shape[0]),self.C*np.ones(X.shape[0])))
-            h = h.reshape(-1, 1)
-            b = 0
-            solvers.options['show_progress'] = False
-            sol = solvers.qp(matrix(P),matrix(q),matrix(G),matrix(h),matrix(A, tc='d'),matrix(b,tc='d'))
-            self.alphas = np.array(sol['x']).flatten()
-    def predict(self, X):
-        pass
+        else:
+            K = np.array([[((self.gamma*np.dot(X[i],X[j])+self.coeff0)**self.deg)
+                       for j in range(X.shape[0])]
+                          for i in range(X.shape[0])])
+        self.K = K
+        Y = np.outer(y,y)
+        P = Y*K
+        q = -np.ones(X.shape[0])
+        A = y.reshape(1,-1)
+        G = np.vstack((-np.eye(X.shape[0]),np.eye(X.shape[0])))
+        h = np.hstack((np.zeros(X.shape[0]),self.C*np.ones(X.shape[0])))
+        h = h.reshape(-1, 1)
+        b = 0
+        solvers.options['show_progress'] = False
+        sol = solvers.qp(matrix(P),matrix(q),matrix(G),matrix(h),matrix(A, tc='d'),matrix(b,tc='d'))
+        self.alphas = np.array(sol['x']).flatten()
+        t = (self.alphas*y*X.T).T
+        self.w = np.zeros(X.shape[1])
+        for i in range(X.shape[0]):
+            self.w += t[i]
+
+        b_candidates = []
+        for i in range(len(self.alphas)):
+            if self.alphas[i] > 0:
+                b_candidates.append(y[i]-np.dot(self.w, X[i]))
+        self.b = np.mean(b_candidates)
+
+
+    def predict(self, X_test):
+        X_test = np.array(X_test)
+        value = -self.b*np.ones(X_test.shape[0])
+        for i in range(X_test.shape[0]):
+            for j in range(self.X.shape[0]):
+                if self.kernel == 'rbf':
+                    value[i] += (self.alphas[j]*self.Y[j]*
+                                 np.exp(-self.gamma*np.sum((self.X[j]-X_test[i])**2)))
+                else:
+                    value[i] += (self.alphas[j] * self.Y[j] *
+                                 (self.gamma * np.dot(self.X[j], X_test[i])+self.coeff0) ** self.deg)
+        return np.sign(value)
--- main.py
+++ main.py
@@ -1,10 +1,80 @@
+from numpy.ma.core import shape
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
 from SVM import SVM
+from ucimlrepo import fetch_ucirepo
+import numpy as np
+
+def load_split_data():
+    wine_quality = fetch_ucirepo(id=186)
+
+    X = wine_quality.data.features.to_numpy()[:600]
+    y = wine_quality.data.targets.to_numpy().ravel()[:600]
+    y[y<5] = -1
+    y[y>=5] = 1
+    test_fraction = 0.3
+    test_number = int(test_fraction * X.shape[0])
+    indices = np.random.permutation(X.shape[0])
+    test_indices = indices[:test_number]
+    train_indices = indices[test_number:]
+    X_train = X[train_indices]
+    y_train = y[train_indices]
+    X_test = X[test_indices]
+    y_test = y[test_indices]
+    return X_train, y_train, X_test, y_test
+
+def calculate_metrics(y_pred, y_test):
+    TP = 0
+    TN = 0
+    FP = 0
+    FN = 0
+    for i in range(y_pred.shape[0]):
+        if y_pred[i] == 1:
+            if y_test[i] == 1:
+                TP += 1
+            else:
+                FP += 1
+        else:
+            if y_test[i] == 1:
+                FN += 1
+            else:
+                TN += 1
+    accuracy = (TP + TN) / (TP + TN + FP + FN)
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1_score = (2 * precision * recall) / (precision + recall)
+    return accuracy, precision, recall, f1_score
 
 def test_svm():
-    svm = SVM(kernel='rbf', gamma=1, C=1)
-    X = [[1, 2], [2, 3], [3, 3]]
-    y = [1, 1, -1]
-    svm.fit(X,y)
+
+    X_train, y_train, X_test, y_test = load_split_data()
+    svm = SVM(kernel='poly', gamma=0.3, C=10.0, coeff0=1, deg=2)
+    svm.fit(X_train, y_train)
+    y_pred = svm.predict(X_test)
+    accuracy, precision, recall, f1_score = calculate_metrics(y_pred, y_test)
+    print("My SVM")
+    print("Accuracy: ", accuracy)
+    print("Precision: ", precision)
+    print("Recall: ", recall)
+    print("F1-score: ", f1_score)
+
+def test_svm_sklearn():
+    X_train, y_train, X_test, y_test = load_split_data()
+
+    svm = SVC(kernel='poly', gamma=0.3, C=10, coef0=1, degree=2)
+    svm.fit(X_train, y_train)
+    svm.fit(X_train, y_train)
+
+    y_pred = svm.predict(X_test)
+
+    accuracy, precision, recall, f1_score = calculate_metrics(y_pred, y_test)
+    print("Scikit SVM")
+    print("Accuracy: ", accuracy)
+    print("Precision: ", precision)
+    print("Recall: ", recall)
+    print("F1-score: ", f1_score)
 
 if __name__ == '__main__':
     test_svm()
+    test_svm_sklearn()
